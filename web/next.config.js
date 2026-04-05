468168d59d7ff4134640a13876dbe483d515080f
Nidhicodes <nidhisingh200418@gmail.com>
2026-04-06 03:24:18 +0530

    fix: fixed next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     NEXT_PUBLIC_RPC_URL: process.env.RPC_URL || "https://evmrpc-testnet.0g.ai",
     NEXT_PUBLIC_INFT_ADDRESS: process.env.INFT_CONTRACT_ADDRESS || "",
     NEXT_PUBLIC_REGISTRY_ADDRESS: process.env.MEMORY_REGISTRY_ADDRESS || "",
+    NEXT_PUBLIC_MARKETPLACE_ADDRESS: process.env.MARKETPLACE_ADDRESS || "",
     NEXT_PUBLIC_STORAGE_INDEXER: process.env.STORAGE_INDEXER_RPC || "https://indexer-storage-testnet-turbo.0g.ai",
     NEXT_PUBLIC_CHAIN_ID: process.env.CHAIN_ID || "16602",
   },
